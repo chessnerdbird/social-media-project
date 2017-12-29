009887ab7186235e41b0effb84ba7bbf71714027
David Blackwelder <dblackw2@gmail.com>
2017-12-28 22:53:18 -0500

    Update routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   #User has many tweets and when a User is destroyed the associated tweets get destroyed also
   has_many :tweets, dependent: :destroy
+  serialize :following, Array
 
   validates :username, presence: true, uniqueness: true
 
