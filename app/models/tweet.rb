99381cdab184a91b44590970225ca5776b61fe6f
David Blackwelder <dblackw2@gmail.com>
2017-12-29 00:39:37 -0500

    Shorten URL

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,10 +1,35 @@
 class Tweet < ApplicationRecord
 
+	before_validation :link_check
+
 	belongs_to :user
 
 	has_many :tweet_tags
 	has_many :tags, through: :tweet_tags
 
 	validates :message, presence: true, length: {maximum: 140, too_long: " A tweet is only 140 max. Everybody knows that!"}
+
+	private 
 	
+	def link_check
+
+		if self.message.include? "http://"
+			check = true 
+		elsif self.message.include? "https://"
+			check = true
+		else
+			check = false
+		end
+
+		if check == true
+			arr = self.message.split
+			index = arr.map{ |x| x.include? "http"}.index(true)
+			self.link = arr[index]
+			if arr[index].length > 23
+				arr[index] = "#{arr[index][0..20]}..."
+			end
+
+			self.message = arr.join(" ")
+		end
+	end	
 end
